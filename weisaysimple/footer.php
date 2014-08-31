b829d78d2a2f83e7b898ecac5ec4b021869b6d93
latelee <latelee@163.com>
2014-08-31 14:56:48 +0800

    添加底部版权声明。
    Signed-off-by: latelee <latelee@163.com>

--- weisaysimple/footer.php
+++ weisaysimple/footer.php
@@ -2,6 +2,8 @@
 <div id="footer">Copyright <?php echo comicpress_copyright(); ?> <a href="<?php bloginfo('siteurl'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>. Powered by <a href="http://www.wordpress.org/" rel="external">WordPress</a>.
 <?php wp_footer(); ?>
 <a href="http://www.miitbeian.gov.cn/" rel="external nofollow">桂ICP备10003672号.</a>
+</p>若非特别声明，本站原创作品均采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/cn/" target=_blank>知识共享署名-非商业性使用-相同方式共享 2.5 中国大陆许可协议</a>进行许可。转载请注明作者及出处。
+</p>This site is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License.
 </div>
 </div>
 <!-- paypal donate -->
