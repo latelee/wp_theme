d52d2da8b8918deceebab040ba53f015d2beba37
latelee <latelee@163.com>
2014-09-01 17:20:47 +0800

    修改404页面。
    Signed-off-by: latelee <latelee@163.com>

--- weisaysimple/404.php
+++ weisaysimple/404.php
@@ -5,21 +5,15 @@
 <div id="mapsite">当前位置： <a title="返回首页" href="<?php echo get_settings('Home'); ?>/">首页</a> &gt; 未知页面</div>
 <div class="left">
 <div class="articles">
-<h3>抱歉，您打开的页面未能找到。<br />
-您可以使用本站的搜索框搜索您想要的内容，如有不便深感抱歉！</h3>
-<p>
-	1、请确保您今天的人品没有问题；</p>
-<p>
-	2、请确保您输入的网址没有错误；</p>
-<p>
-	3、如果以上两点均正常，那么，可能是这篇文章被删除了，或者由于网站改版还没有及时添加该文章；</p>
-<p>
-	4、您可以点击<a href="http://latelee.org">这里</a>返回<a href="http://latelee.org">主页</a>，或者去<a href="http://www.google.com">Google</a>上<a href="http://www.baidu.com">百度</a>一下你要查找的内容；</p>
-<p>
-	5、希望本次出错不会影响您今天的心情，如果是，我们对此表示抱歉并希望得到您的理解。</p>
-<h4>
-	Sorry, but the content you requested could not be found</h4>
-
+<h3>抱歉，您打开的页面未能找到。<br /><br />
+1、请确保您今天的人品没有问题；<br /><br />
+2、请确保您输入的网址没有错误；<br /><br />
+3、如果以上两点均正常，那么，可能是这篇文章被删除了，或者由于网站改版还没有及时添加该文章；<br /><br />
+4、如果你是从其它网站找原始出处而来到这里，首先表示感谢，你可以在右上方的搜索框里搜索文章标题，一般能找到；<br /><br />
+5、您可以点击<a href="http://latelee.org">这里</a>返回<a href="http://latelee.org">主页</a>，或者去<a href="http://www.google.com">Google</a>上<a href="http://www.baidu.com">百度</a>一下你要查找的内容；<br /><br />
+6、希望本次出错不会影响您今天的心情，如果是，我们对此表示抱歉并希望得到您的理解。<br /><br />
+Sorry, but the content you requested could not be found.</h4>
+</h3>
 </div>
 </div>
 </div>
