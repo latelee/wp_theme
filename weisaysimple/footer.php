8b05a9c564dbe05606d55252f540d77d3a8db665
latelee <latelee@163.com>
2014-09-03 08:55:12 +0800

    更改版权为3.0.
    Signed-off-by: latelee <latelee@163.com>

--- weisaysimple/footer.php
+++ weisaysimple/footer.php
@@ -1,8 +1,8 @@
 <div class="clear"></div>
 <div id="footer">Copyright &copy; 2010-2014 <a href="http://www.latelee.org/" title="迟思堂工作室">迟思堂工作室</a>.  &nbsp; Powered by <a href="http://www.wordpress.org/" rel="external">WordPress</a>. Theme by <a href="http://www.weisay.com/" rel="external">Weisay</a>. Modified by <a href="http://www.latelee.org/">Late Lee</a>.
 <a href="http://www.miitbeian.gov.cn/" rel="external nofollow"> 桂ICP备10003672号.</a>
-</p>若非特别声明，本站原创作品均采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/cn/" target=_blank>知识共享署名-非商业性使用-相同方式共享 2.5 中国大陆许可协议</a>进行许可。转载请注明作者及出处。
-</p>This site is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 2.5 License.
+</p>若非特别声明，本站原创作品均采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/cn/" target=_blank>知识共享署名-非商业性使用-相同方式共享 3.0 中国大陆许可协议</a>进行许可。转载请注明作者及出处。
+</p>This site is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License.
 </div>
 </div>
 <div style="text-align: center; margin-top:5px;">
