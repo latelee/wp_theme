8c74cff401dcbf8b0b909ac982c5842158c1e899
latelee <latelee@163.com>
2014-09-03 19:25:49 +0800

    完善404页面。 将底部的paypal图标改成链接。
    Signed-off-by: latelee <latelee@163.com>

--- weisaysimple/404.php
+++ weisaysimple/404.php
@@ -9,9 +9,11 @@
 1、请确保您今天的人品没有问题；<br /><br />
 2、请确保您输入的网址没有错误；<br /><br />
 3、如果以上两点均正常，那么，可能是这篇文章被删除了，或者由于网站改版还没有及时添加该文章；<br /><br />
-4、如果你是从其它网站找原始出处而来到这里，首先表示感谢，你可以在右上方的搜索框里搜索文章标题，一般能找到；<br /><br />
-5、您可以点击<a href="http://latelee.org">这里</a>返回<a href="http://latelee.org">主页</a>，或者去<a href="http://www.google.com">Google</a>上<a href="http://www.baidu.com">百度</a>一下你要查找的内容；<br /><br />
-6、希望本次出错不会影响您今天的心情，如果是，我们对此表示抱歉并希望得到您的理解。<br /><br />
+4、如果你是从其它网站找原始出处而来到这里，首先表示感谢，请您尝试去掉在网页后面地址出现的“数字-”，<br />
+如地址后面是“99-boa-test.html ”，把“99-”去掉；<br /><br />
+5、如果你通过搜索引擎找文章而到了这个页面，你可以在本网站搜索框里搜索文章标题，一般能找到；<br /><br />
+6、您可以点击<a href="http://www.latelee.org">这里</a>返回<a href="http://www.latelee.org">主页</a>，或者去<a href="http://www.google.com">Google</a>上<a href="http://www.baidu.com">百度</a>一下你要查找的内容；<br /><br />
+7、希望本次出错不会影响您今天的心情，如果是，我们对此表示抱歉并希望得到您的理解。<br /><br />
 Sorry, but the content you requested could not be found.</h4>
 </h3>
 </div>
--- weisaysimple/footer.php
+++ weisaysimple/footer.php
@@ -1,18 +1,11 @@
 <div class="clear"></div>
 <div id="footer">Copyright &copy; 2010-2014 <a href="http://www.latelee.org/" title="迟思堂工作室">迟思堂工作室</a>.  &nbsp; Powered by <a href="http://www.wordpress.org/" rel="external">WordPress</a>. Theme by <a href="http://www.weisay.com/" rel="external">Weisay</a>. Modified by <a href="http://www.latelee.org/">Late Lee</a>.
 <a href="http://www.miitbeian.gov.cn/" rel="external nofollow"> 桂ICP备10003672号.</a>
+<?php if (get_option('swt_tj') == 'Display') { ?><?php echo stripslashes(get_option('swt_tjcode')); ?><?php { echo '.'; } ?>	<?php } else { } ?>
+<a href="http://www.latelee.org/donate">赞助本站</a>
 </p>若非特别声明，本站原创作品均采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/cn/" target=_blank>知识共享署名-非商业性使用-相同方式共享 3.0 中国大陆许可协议</a>进行许可。转载请注明作者及出处。
 </p>This site is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License.
 </div>
-</div>
-<div style="text-align: center; margin-top:5px;">
-    <form action="https://www.paypal.com/cgi-bin/webscr" target="_blank"  method="post">
-        <input type="hidden" name="cmd" value="_donations">
-        <input type="hidden" name="business" value="latelee@163.com">
-        <input type="hidden" name="item_name" value="Thank you for your generosity, may god bless you">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-    </form>
 </div>
 </body>
 </html>
